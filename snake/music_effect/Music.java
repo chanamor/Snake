925b171c1a8812bd9bca0e6da8902cc500ff25a8
chanamor <teepakorn.san@ku.th>
2025-10-12 21:13:47 +0700

    Add files via upload

--- snake/music_effect/Music.java
+++ snake/music_effect/Music.java
@@ -1,5 +1,36 @@
 package music_effect;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Music {
-    
+    private Clip clip;
+
+
+    public void playMusic(String filePath , boolean loop){
+
+        try {
+            File musicPath = new File(filePath);
+           if (musicPath.exists()) {
+             AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+             clip = AudioSystem.getClip();
+             clip.open(audioInput);
+             clip.start();
+             if (loop) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+             }else{
+                System.out.println("No music"+filePath);
+             }
+           }
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopMusic(){
+        if (clip != null && clip.isRunning()) {
+            clip.stop();
+        }
+    }
 }
